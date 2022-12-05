f69850f52bdb0f84d0a74022c9cf33184994ca79
Patrick Lam Nguyen <patricklamnguyen@gmail.com>
2022-12-05 00:15:02 -0500

    Updated tailwind config with screen breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ module.exports = {
       "./app/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
-      extend: {},
+        screens: {
+            sm: '480px',
+            md: '768px',
+            lg: '976px',
+            xl: '1440px',
+        },
+        color: {
+            
+        },
+        extend: {},
     },
     plugins: [
         require('@tailwindcss/typography'),
